c4d7ca544dc0b5c00cc64540335c2623d3d345c1
Your Name <carlosguzmantejeda13@gmail.com>
2025-02-16 17:58:37 -0500

    done

--- sliding windows/38.1.py
+++ sliding windows/38.1.py
@@ -1,2 +1,21 @@
+
+def most_weekly_sales(sales):
+    l = 0
+    r = 0
+
+    window_sum = 0
+    cur_max = 0
+
+    while r < len(sales):
+        window_sum += sales[r]
+        r += 1
+        if r - l == 7:
+            cur_max = max(cur_max, window_sum)
+            window_sum -= sales[l]
+            l += 1
+    return cur_max
+
+
 sales = [0, 3, 7, 12, 10, 5, 0, 1, 0, 15, 12, 11, 1]
 
+print(most_weekly_sales(sales))
